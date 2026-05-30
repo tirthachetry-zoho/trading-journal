89f146d98559e07f975997d7db9aef6d3920774d
Tirtha Chetry <tirthachetry@zohomail.in>
2026-05-30 15:05:36 +0530

    Clean up next.config.ts by removing duplicates
    Removed duplicate nextConfig definitions.

--- next.config.ts
+++ next.config.ts
@@ -5,24 +5,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
