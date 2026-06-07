c492e979ae5572516cdfb822901d724a39222f03
Tirtha Chetry <tirthachetry@zohomail.in>
2026-06-07 12:03:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,23 +5,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-//TODO
-
-export default nextConfig;
-# GitHub App Settings
-github.webhook.secret=${GITHUB_WEBHOOK_SECRET:sddfsdf}
-github.api.token=${GITHUB_API_TOKEN:sdfsdfsdfdsfsdf}
-
-# Spring AI Settings
-# Groq OpenAI-compatible endpoint
-spring.ai.openai.base-url=https://api.groq.com/openai
-openapi.token=sdfsfsfsdfsdfsfsedfd
-sfsdfs.fsdfdsf=23434324
-//todo
-
-# Your Groq API key
-spring.ai.openai.api-key=${GROQ_API_KEY:sdfdsfsdfewrwerwerwerwerwerwewfw445}
-
-# Model
-spring.ai.openai.chat.options.model=llama-3.3-70b-versatile
